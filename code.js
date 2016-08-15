beb5d8e2cf176f96601c22e2983f6bf291a8373f
JustinBillings <billings.justinm@gmail.com>
2016-08-15 15:02:45 -0600

    new one

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 var journal = ["DM13 rocks!! Woot!!", "Justin", "Billings"];
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfileName.text
+touch newfileName.text
+
+git clone
+git status
+git add fileName
+git commit -m
+git push
+
+*/
